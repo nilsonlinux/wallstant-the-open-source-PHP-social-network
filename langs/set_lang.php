4c23ec2a4d449c5a539fafb353a5b077502a5843
Nilsonlinux <nilsonlinux@gmail.com>
2018-07-19 14:21:10 -0300

    Update set_lang.php

--- langs/set_lang.php
+++ langs/set_lang.php
@@ -6,6 +6,9 @@ case 'English':
 case 'العربية':
 	include_once $path."langs/arabic.php";
 	break;
+case 'PortuguêsBR':
+	include_once $path."langs/portugues.php";
+	break;
 default:
 	$_SESSION['language'] = "English";
 	include_once $path."langs/english.php";
